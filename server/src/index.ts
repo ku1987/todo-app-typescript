714b1c447173568dbdf1db613f61eab37500e309
ku1987 <kei.usami1987@gmail.com>
2021-02-25 16:43:45 +0900

    Add .prettierrc

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import morgan from "morgan";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
-import healthRouter from "./routers/health";
+import healthRouter from './routers/health';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "20mb" }));
+app.use(express.json({ limit: '20mb' }));
 app.use(cors());
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.use(healthRouter());
 
